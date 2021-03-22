3e4a92cccf197a1fe59483d0acd932d72d557c73
iurisegtovich <iurisegtovich@gmail.com>
2021-03-22 20:21:52 -0300

    gaussnewtonlinesearch

--- pytherm_lib_pkg/optimization_pkg/hard_quasi_newton_mod.py
+++ pytherm_lib_pkg/optimization_pkg/hard_quasi_newton_mod.py
@@ -71,8 +71,8 @@ def minimize_linesearch(objF,x0,step):
                 a=a,
                 b=c)
     
-    print(alpha)
-    input('alpha')
+    #print(alpha)
+    #input('alpha')
     return alpha
 
 def gss(f,a,b):
@@ -171,3 +171,109 @@ if __name__ == '__main__':
 #linesearch
 #prints
 #asserts
+
+def GaussNewton_STEP(model,par0,XEXP,YEXP,VAREXP,YC,NEXP,NVENT,NVSAI,NPAR):  #NOT TESTED
+    
+    for k in range(NEXP):
+        YC[k,:]=model( XEXP[k,:], par0)
+    
+    DYO=YC-YEXP
+    
+    DFP = np.zeros([NEXP,NVSAI, NPAR])  #versao versatil para evitar vetores muito grandes, pode ser 100,1 ou 1,100 ou 10,10 e o resultado dá igual, só muda a gestão de mem
+    
+    
+    for k in range(NEXP):
+        Lmodel = lambda par: model( XEXP[k,:], par, )
+        Lmygrad=nd.Gradient( Lmodel ) #grad lambda function
+        mygrad=Lmygrad(par0) #grad call at x0
+        print(mygrad)
+        #input('mygrad')
+        DFP[k,:,:] = mygrad #*1
+    
+    U = np.zeros([NPAR])
+    for k in range(NEXP): #POSSO USAR UM FLATTEN EM NEXP*NVSAI
+        U += DFP[k,:,:].T @ ( np.diag(1/VAREXP[k,:]) @ DYO[k,:])
+    # = 1/2 * grad
+    
+    T=np.zeros([NPAR,NPAR]) #ok
+    
+    for k in range(NEXP):
+        T += DFP[k,:,:].T @ ( np.diag(1/VAREXP[k,:]) @ DFP[k,:,:]) #diag(1/v), se fizer 1/diag(v) vai ter termos infinitos
+    
+    cova = np.linalg.inv(T) # = 2 * hessinv
+    
+    delP = - cova @ U.T #=2hesinv*1/2grad=hesinv*grad    
+    
+    return delP
+
+#wlsq objf for GNs
+def wlsqobjF(model,par0,XEXP,YEXP,VAREXP,YC,NEXP,NVENT,NVSAI,NPAR): #NOT TESTED
+    #give YC memory
+    f=0
+    for k in range(NEXP):
+        YC[k,:]=model( XEXP[k,:], par0)
+        DYk=YC[k,:]-YEXP[k,:]
+        f += DYk.T @ np.diag(1/VAREXP[k,:]) @ DYk
+    return f
+
+def GaussNewton_loop(model,x0,XEXP,YEXP,VAREXP,NEXP,NVENT,NVSAI,NPAR): #NOT TESTED
+    
+    YC=np.zeros_like(YEXP)
+    
+    n=len(x0)
+    
+    res=1.
+    tol=1e-4
+    
+    i=0
+    imax=100
+    
+    def objF(par0):
+        return wlsqobjF(model,par0,XEXP,YEXP,VAREXP,YC,NEXP,NVENT,NVSAI,NPAR)    
+    f0=objF(x0)
+    
+    while res>tol and i<imax:
+        xn=x0*1 #copy
+        fn=f0*1 #copy
+        
+        step = GaussNewton_STEP(model,xn,XEXP,YEXP,VAREXP,YC,NEXP,NVENT,NVSAI,NPAR)
+        
+        alpha = minimize_linesearch(objF,xn,step)
+        
+        x0 = xn + step*alpha
+        
+        f0=objF(x0)
+        
+        res = np.linalg.norm(x0-xn)/n #res x
+        #res = np.abs(f0-fn) #res f
+        
+        i+=1
+        return x0
+
+def test2():
+    NEXP=100
+    NVENT=2
+    NVSAI=3
+    NPAR=3
+    XEXP=np.random.random((100,2))
+    YEXP=np.random.random((100,3))
+    VAREXP=np.random.random((100,3))
+    par = np.array([ .8193,.1238,.1245 ])
+    
+    def model(X, par):
+        YC=np.zeros(3)
+        YC[0]=np.exp(X[0]*par[0]) + (X[1]*par[1])**2
+        YC[1]=X[0]*X[1]*par[0]*par[2]
+        YC[2]=X[0]*par[1]/(X[1]*par[2])
+        return YC
+    
+    for k in range(100):
+        YEXP[k,:]=model(XEXP[k,:],par) + np.random.random(3)/100
+        
+    print(YEXP)
+    input('paused')
+    ans=GaussNewton_loop(model,par,XEXP,YEXP,VAREXP,NEXP,NVENT,NVSAI,NPAR) #NOT TESTED
+    print(ans)
+    
+if __name__ == '__main__':
+    test2()
